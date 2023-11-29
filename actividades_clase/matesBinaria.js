7ef9c874085143c305444d505bdea6f8e15f8e97
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2023-11-29 17:15:44 +0100

    updating repo

--- actividades_clase/matesBinaria.js
+++
@@ -1,7 +0,0 @@
-export function binaria(a, b){
-    return a | b;
-}
-
-export function multiplicaBin(a, b){
-    return a & b;
-}
--- app_veterinario/model/animals.js
+++
@@ -1,8 +0,0 @@
-export function animal(id, name, sex, registryNumber, type){
-    this.id = id;
-    this.name = name;
-    this.sex = sex;
-    this.registryNumber = registryNumber;
-    this.type = type;
-
-}
