7960167c5b809b4be61af50fcc443ac6acc614d5
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2024-01-12 19:49:10 +0100

    updating repo

--- kittens_md/src/js/application.js
+++ kittens_md/src/js/application.js
@@ -1,14 +1,17 @@
 import $ from './jquery-2.1.0.min.js'
-import '/src/sass/styles.scss'; // Importa tu archivo Sass principal
+import '/src/sass/styles.scss';
+
 import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCTabScroller} from '@material/tab-scroller';
 import {MDCRipple} from '@material/ripple';
 
 
 
+const tabScroller = new MDCTabScroller(document.querySelector('.mdc-tab-scroller'));
+
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
-
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
 const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
@@ -16,6 +19,7 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
 
+console.log("hola");
 var names = ['Tiger', 'Samuel', 'Leeloo',
              'Bella', 'Mo', 'Sly',
              'Beezy', 'Maple', 'Rodney',
@@ -26,15 +30,16 @@ var ages  = ['9 Weeks', '12 Weeks', '3 Months',
              '4 Months', '2 Months', '14 Weeks',
              '6 Months', '10 Weeks', '8 Months'];
 
-$('.kittens').find('.mdc-card__media-content').each( function(i, el) {
-  var img = $(el).find('img');
-  var name = $(el).closest('.kittens').find('.name'); // Select the name span within the same .kittens div
-  var age = $(el).closest('.kittens').find('.age'); // Select the name span within the same .kittens div
-
-  var w = 250;
-  var h = 250;
-
-  img.attr('src', 'http://placekitten.com/'+w+'/'+h+'?image='+i);
-  name.text(names[i]);
-  age.text(ages[i]);
+             $('.kittens').find('.mdc-card').each( function(i, el) {
+              var img = $(el).find('.kitten');
+              var name = $(el).find('.name');
+              var age = $(el).find('.age');
+            
+              var w = 250;
+              var h = 250;
+            
+              img.attr('src', 'http://placekitten.com/'+w+'/'+h+'?image='+i);
+              name.text(names[i]);
+              age.text(ages[i]);
 });
+            
